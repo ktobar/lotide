cb712ea8ae31f427bd89d3fce92fe5b726fd4a70
Kelvin <tobar411@gmail.com>
2020-10-28 01:22:49 +0000

    added eqArrays.js file

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual !==  expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else if (actual ===  expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  let trueCount = 0;
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] === arrayTwo[i]) {
+      trueCount++;
+    }
+  }
+  if (trueCount === arrayOne.length && trueCount === arrayTwo.length) {
+    return true;
+  } else {
+    return false;
+  }
+};
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
