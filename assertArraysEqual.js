dd9a9cb14975c1cfd1f4c512781f006ca668392c
Kelvin <tobar411@gmail.com>
2020-10-28 02:20:33 +0000

    added assertArraysEqual.js file

---
+++ assertArraysEqual.js
@@ -0,0 +1,27 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  let trueCount = 0;
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] === arrayTwo[i]) {
+      trueCount++;
+    }
+  }
+  if (trueCount === arrayOne.length && trueCount === arrayTwo.length) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  let result = eqArrays(arrayOne,arrayTwo);
+
+  if (result) {
+    console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([6, 4, 7], [4, 1, 6]);
+assertArraysEqual([1, 2, 3], [1, 2, "3"]);
