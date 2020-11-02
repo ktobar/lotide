7bb4184f9804e6d55fbf9bfa32841a6c116d2371
Kelvin <tobar411@gmail.com>
2020-11-02 19:30:35 +0000

    refactor eqArraysTest with module and testing file

--- eqArrays.js
+++ eqArrays.js
@@ -19,9 +19,5 @@ const eqArrays = function(arrayOne, arrayTwo) {
     return false;
   }
 };
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
